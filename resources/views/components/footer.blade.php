3d07f70dbce0f0b588e3aed05b815d02d6467ffd
motoshifurugen <amanogawa_m@icloud.com>
2020-05-25 12:27:13 +0900

    fix footer and add viewport setting

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="py-3 bg-light">
+<footer class="py-3 bg-light fixed-bottom">
     <div class="container">
         <span class="text-muted small">In Out app by Laravel 7 & Bootstrap 4</span>
     </div>
